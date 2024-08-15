5a2a83e08e8735ff573c28fd5d5c2ad7f3f0a2ba
Tiago tonon <89621378+NotoriosT@users.noreply.github.com>
2024-08-15 14:23:38 -0300

    Update script_to_convert_sarif_to_html.py

--- script_to_convert_sarif_to_html.py
+++ script_to_convert_sarif_to_html.py
@@ -1,19 +1,6 @@
-import json
-from sarif_om import SarifLog
-
-def get_code_snippet(file_path, line_number, context=2):
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            start_line = max(line_number - context - 1, 0)
-            end_line = min(line_number + context, len(lines))
-            return ''.join(lines[start_line:end_line])
-    except Exception as e:
-        return f"Could not retrieve code snippet: {str(e)}"
-
 def sarif_to_html(sarif_file, output_html):
-    with open(sarif_file, "r") as sarif_file:
-        sarif_data = json.load(sarif_file)
+    with open(sarif_file, "r") as file:
+        sarif_data = json.load(file)
         sarif_log = SarifLog.from_dict(sarif_data)
         
     html_content = "<html><body><h1>Vulnerability Report</h1>"
@@ -40,4 +27,5 @@ def sarif_to_html(sarif_file, output_html):
     with open(output_html, "w") as html_file:
         html_file.write(html_content)
 
-sarif_to_html("results/java.sarif", "relatorio_vulnerabilidades.html")
+# Altere o caminho para apontar diretamente para o arquivo SARIF
+sarif_to_html("results/java.sarif/java.sarif", "relatorio_vulnerabilidades.html")
